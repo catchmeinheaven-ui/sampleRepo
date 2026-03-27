54aa627b462928aac83a5c8da03ed37afdd5293f
catchmeinheaven-ui <catchmeinheaven@gmail.com>
2026-03-27 16:52:12 +0530

    Update AcmGlobal.js

--- icm-builder/WebContent/designer/AcmGlobal.js
+++ icm-builder/WebContent/designer/AcmGlobal.js
@@ -17,7 +17,7 @@ define(function () {
 	var AcmGlobal = {
 		// Reference of SolutionHomw Widget
 		solutionHome				: null,
-		designerHome				: null,
+		designerHome				: null
 		// Reference of first invalid field
 		invalidField				: null,
 		propsDetails				: null,
